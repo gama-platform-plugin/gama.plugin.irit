5242b5b8ba33d4985f2c3d101220fdc0dd771778
rubenanko <rubenanko@gmail.com>
2026-06-02 11:36:40 +0700

    ci: maven build and dependency cache

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ fi
 
 cd "${ROOT}/gama.plugin.parent"
 mvn clean install -B -e \
+    -Dmaven.build.cache.configPath="maven-build-cache-config.xml" \
     -Dgama.p2.version="${GAMA_P2_VERSION}" \
     -Ddeploy.subdir="${PLUGIN_REPO_NAME}" \
     -Dtycho.p2.transport.min-cache-minutes=0 \
